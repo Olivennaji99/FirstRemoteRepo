1b1ef464ba1834d7ea53b25bebf13bb0c9571c51
Olive Nnaji <olivennaji99@gmail.com>
2025-02-10 17:06:03 +0100

    My first shell script version 1

---
+++ first-shell-script.sh
@@ -0,0 +1,16 @@
+#!|bin|bash
+
+##########################
+#Author : Olive Nnaji
+#Date :10-02-2025
+#
+#This script should echo my nme
+#
+#
+#
+echo "My name is Olive Nnaji"
+#
+#version 1
+#
+#####################
+
